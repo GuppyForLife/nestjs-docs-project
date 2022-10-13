28387ed07a351656b3521007e28a6bcd2765be44
Matthew Jovenal <matthewjovenal1@outlook.com>
2022-10-13 11:00:03 -0700

    Wired provider and tested in postman

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './controllers/cats.controller';
+import { CatService } from './providers/cats.service';
 
 @Module({
   controllers: [CatsController],
+  providers: [CatService],
 })
 export class AppModule {}
